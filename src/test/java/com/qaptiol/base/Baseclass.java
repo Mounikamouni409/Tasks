b00b11f596d28f8f5243a6923626b8c83b2bcc39
Mounikamouni409 <patnammounika640@gmai.com>
2024-11-11 14:13:48 +0530

    Code pushed completed

--- src/test/java/com/qaptiol/base/Baseclass.java
+++ src/test/java/com/qaptiol/base/Baseclass.java
@@ -1,21 +1,29 @@
 package com.qaptiol.base;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class Baseclass {
 	
+	
 	public static WebDriver driver;
 	
 	@BeforeClass
-	public void openBrowser() {
+	public void openBrowser() throws IOException {
+		
+		FileReader file = new FileReader("C:\\Users\\Qapitol QA\\eclipse-workspace\\Qapitol\\src\\test\\resources\\TestData.properties");
+		Properties props = new Properties();
+		props.load(file);
+		
 		
 		driver = new ChromeDriver();
-		driver.get("https://demoqa.com/");
+		driver.get(props.getProperty("url"));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
